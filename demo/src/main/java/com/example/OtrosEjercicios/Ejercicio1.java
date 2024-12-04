e1f6c0bbc80a4d8c0c048331b9749106f0678295
Enrique Muñoz-Torrero <enriquemtr8@gmail.com>
2024-12-04 21:28:05 +0100

    He comentado el ejercicio1.java de la carpeta de OtrosEjercicios

--- demo/src/main/java/com/example/OtrosEjercicios/Ejercicio1.java
+++ demo/src/main/java/com/example/OtrosEjercicios/Ejercicio1.java
@@ -10,16 +10,21 @@ public class Ejercicio1 {
         //Creamos un scanner para introducir datos desde nuestra terminal.
         Scanner sc = new Scanner(System.in);
 
+        //Creamos nuestras dos variables
         double radio = 0;
         double area = 0;
 
+        //Introducimos en terminal cual es el radio de nuestro circulo
         System.out.println("Introduce el radio de tu circulo: ");
         radio = sc.nextDouble();
 
+        //Creamos la formula que nos dara el resultado del area de nuestro circulo
         area = Math.pow((Math.PI * radio), 2);
 
+        //Imprimimos por pantalla un mensaje mas el valor de nuestra area
         System.out.println("El area del circulo es de " + area);
         
+        //Cerramos el scanner.
         sc.close();
     }
 }
